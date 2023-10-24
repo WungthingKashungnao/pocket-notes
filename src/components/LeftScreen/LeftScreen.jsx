787f5df67ca6601f281fbcf05e36299a3b624c7b
wungthing <wungthingkashungnao123@gmail.com>
2023-10-24 08:55:19 +0530

    set up

---
+++ src/components/LeftScreen/LeftScreen.jsx
@@ -0,0 +1,7 @@
+import style from "./leftscreen.module.css";
+
+const LeftScreen = () => {
+  return <div className={style.container}>Left</div>;
+};
+
+export default LeftScreen;
---
+++ src/components/RightScreen.jsx/RightScreen.jsx
@@ -0,0 +1,7 @@
+import styles from "./rightscreen.module.css";
+
+const RightScreen = () => {
+  return <div className={styles.container}>Right</div>;
+};
+
+export default RightScreen;
